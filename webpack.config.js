27f969753589ba2b25dc4e50545b9f7f642e9754
Toily <escusado@gmail.com>
2017-08-03 00:27:56 -0500

    Add scss support

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,20 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader"
+        }, {
+            loader: "css-loader"
+        }, {
+            loader: "sass-loader",
+            options: {
+                includePaths: ["absolute/path/a", "absolute/path/b"]
+            }
+        }]
+      }
     ]
   },
   plugins: [HtmlWebpackPluginConfig]
